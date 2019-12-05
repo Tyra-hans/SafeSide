cd88655975916cb59b5d5894c352e5f1cda62777
Tyra-hans <tyrahans17@gmail.com>
2019-12-05 10:13:53 +0300

    Changes the database to postgres to allow some data to be saved

--- safeproj/settings.py
+++ safeproj/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'safeproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'safeside',
+        'USER': 'tyra',
+        'PASSWORD': 'pasword',
+
     }
 }
 
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
