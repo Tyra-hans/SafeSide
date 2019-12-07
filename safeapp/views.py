8957720d5a4366769994b673cc872a0f98f99f32
Tyra-hans <tyrahans17@gmail.com>
2019-12-07 21:04:52 +0300

    Adds functiomality to consume the api amd displays its information when pronted out

--- safeapp/views.py
+++ safeapp/views.py
@@ -7,12 +7,14 @@ import urllib.request
 def home(request):
     if request.method == 'POST':
         city = request.POST['city']
+        
 
     # url='https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name={}&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg'.format(city)
-
-        source = urllib.request.urlopen( 
-            'https://openweathermap.org/data/2.5/weather?q=' 
-                    + city + '&appid = b6907d289e10d714a6e88b30761fae22').read() 
+        # source = urllib.request.urlopen('https://openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22').read() 
+        source =   urllib.request.urlopen('https://openweathermap.org/data/2.5/weather?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22').read()
+        # source = urllib.request.urlopen( 
+            # 'https://openweathermap.org/data/2.5/weather?q=' 
+                    # + city + '&appid = b6907d289e10d714a6e88b30761fae22').read() 
 #   'https://openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22'.format(city)
 
 # converting JSON data to a dictionary 
