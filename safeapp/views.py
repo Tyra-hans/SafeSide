a565456fb16cf251368e54f4cc9ae3ae2aa8b439
Tyra-hans <tyrahans17@gmail.com>
2019-12-10 09:07:21 +0300

    Adds error handling for when the user searches a city thats unavailable

--- safeapp/views.py
+++ safeapp/views.py
@@ -5,41 +5,122 @@ import urllib.request
 from pprint import pprint
 from django.http import HttpResponseServerError
 
+# def home(request):
+#     if request.method == 'POST':
+#         city = request.POST['city']
+        
+#         source =   urllib.request.urlopen('https://openweathermap.org/data/2.5/weather?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22').read()
+#         source2 = urllib.request.urlopen('https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name='+city+'&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg').read()
+    
+# # converting JSON data to a dictionary 
+#         list_of_data = json.loads(source) 
+#         list_of_data2 = json.loads(source2)
+#         # print(list_of_data)
+#         try:
+#             data = {
+               
+#             "country_code": str(list_of_data['sys']['country']), 
+#             "coordinate": str(list_of_data['coord']['lon']) + ' '
+#                         + str(list_of_data['coord']['lat']), 
+#             "temp": str(list_of_data['main']['temp']) + 'k', 
+#             "pressure": str(list_of_data['main']['pressure']), 
+#             "humidity": str(list_of_data['main']['humidity']), 
+#             }
+#         except ValueError as e:
+#             print('error occured')
+
+#         finally:
+#             print('Dummy data resp')
+
+#         # data = { 
+#         # try:
+#         #     "country_code": str(list_of_data['sys']['country']), 
+#         #     "coordinate": str(list_of_data['coord']['lon']) + ' '
+#         #                 + str(list_of_data['coord']['lat']), 
+#         #     "temp": str(list_of_data['main']['temp']) + 'k', 
+#         #     "pressure": str(list_of_data['main']['pressure']), 
+#         #     "humidity": str(list_of_data['main']['humidity']), 
+#         # except:
+#         #     "alert":"Something occured"
+#         # } 
+        
+#         # print(data)
+#         print(list_of_data2) 
+#         if list_of_data2['alerts']['alerts'] == []:
+#             data2 = {
+#                 "city": str(list_of_data2['alerts']['city']),
+#                 "alerts": "Weather is not extreem in " + city
+
+#             }
+#         # print(list_of_data2)
+#         else:
+#             data2 = {
+#                 "city": str(list_of_data2['alerts']['city']),
+#                 "alerts": str(list_of_data2['alerts']['alerts'][0]['description'] + '. Stay safe')
+
+#             }
+#     else:
+#         data={}
+#         data2={}
+
+
+#     return render(request,'weather/home.html',{'data':data, 'data2' :data2})
+
+
+
 def home(request):
     if request.method == 'POST':
-        city = request.POST['city']
         
-        source =   urllib.request.urlopen('https://openweathermap.org/data/2.5/weather?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22').read()
-        source2 = urllib.request.urlopen('https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name='+city+'&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg').read()
+    # print(list_of_data)
+        try:
+            city = request.POST['city']
     
+            source =   urllib.request.urlopen('https://openweathermap.org/data/2.5/weather?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22').read()
+            source2 = urllib.request.urlopen('https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name='+city+'&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg').read()
+
 # converting JSON data to a dictionary 
-        list_of_data = json.loads(source) 
-        list_of_data2 = json.loads(source2)
-        # print(list_of_data)
-       
-        
-        data = { 
+            list_of_data = json.loads(source) 
+            list_of_data2 = json.loads(source2)
+            data = {
+            
             "country_code": str(list_of_data['sys']['country']), 
             "coordinate": str(list_of_data['coord']['lon']) + ' '
-                        + str(list_of_data['coord']['lat']), 
+                    + str(list_of_data['coord']['lat']), 
             "temp": str(list_of_data['main']['temp']) + 'k', 
             "pressure": str(list_of_data['main']['pressure']), 
             "humidity": str(list_of_data['main']['humidity']), 
-        } 
-        
-        # print(data)
-        print(list_of_data2) 
+            }
+        except ValueError as e:
+            print('error occured')
+
+        finally:
+            print('Dummy data resp')
+
+    # data = { 
+    # try:
+    #     "country_code": str(list_of_data['sys']['country']), 
+    #     "coordinate": str(list_of_data['coord']['lon']) + ' '
+    #                 + str(list_of_data['coord']['lat']), 
+    #     "temp": str(list_of_data['main']['temp']) + 'k', 
+    #     "pressure": str(list_of_data['main']['pressure']), 
+    #     "humidity": str(list_of_data['main']['humidity']), 
+    # except:
+    #     "alert":"Something occured"
+    # } 
+    
+    # print(data)
+    # print(list_of_data2) 
         if list_of_data2['alerts']['alerts'] == []:
             data2 = {
                 "city": str(list_of_data2['alerts']['city']),
                 "alerts": "Weather is not extreem in " + city
 
             }
-        # print(list_of_data2)
+    # print(list_of_data2)
         else:
             data2 = {
                 "city": str(list_of_data2['alerts']['city']),
-                "alerts": str(list_of_data2['alerts']['alerts'][0]['description'])
+                "alerts": str(list_of_data2['alerts']['alerts'][0]['description'] + '. Stay safe')
 
             }
     else:
