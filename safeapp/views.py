4cb7d1ea61b7c3f18ff27e500619846e6b173cdd
Tyra-hans <tyrahans17@gmail.com>
2019-12-10 15:27:42 +0300

    Fixes error handling and allows the home view to detect and catch erors when the status code is not 200

--- safeapp/views.py
+++ safeapp/views.py
@@ -1,55 +1,61 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponseRedirect
 import requests
 import json
 import urllib.request
-from pprint import pprint
-from django.http import HttpResponseServerError
+
 
 def home(request):
+    data={}
+    data={}
     if request.method == 'POST':
         
-    # Try block to catch errors
+    # Try block to catch 
+        city = request.POST['city']
 
         try:
-            city = request.POST['city']
-            # general weather api :
-            source =   urllib.request.urlopen('https://openweathermap.org/data/2.5/weather?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22').read()
-            # weather alerts api :
-            source2 = urllib.request.urlopen('https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name='+city+'&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg').read()
-
-            # converting JSON data to a dictionary 
-            list_of_data = json.loads(source) 
-            list_of_data2 = json.loads(source2)
-            data = {
+            # int('sy')
             
-            "country_code": str(list_of_data['sys']['country']), 
-            "coordinate": str(list_of_data['coord']['lon']) + ' '
-                    + str(list_of_data['coord']['lat']), 
-            "temp": str(list_of_data['main']['temp']) + 'k', 
-            "pressure": str(list_of_data['main']['pressure']), 
-            "humidity": str(list_of_data['main']['humidity']), 
-            }
-        except ValueError as e:
-            print('error occured')
+            source = requests.get('https://openweathermap.org/data/2.5/weather?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22')
+            if not source.status_code//100  == 2:
+            
+                return redirect('error')  
+            source2 = requests.get('https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name='+city+'&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg')
 
-        finally:
-            return redirect('error')
-        # Validation for weather alerts :
-        if list_of_data2['alerts']['alerts'] == []:
-            data2 = {
-                "city": str(list_of_data2['alerts']['city']),
-                "alerts": "Weather is not extreem in " + city
-            }
-        else:
-            data2 = {
-                "city": str(list_of_data2['alerts']['city']),
-                "alerts": str(list_of_data2['alerts']['alerts'][0]['description'] + '. Stay safe')
-            }
-    else:
-        data={}
-        data2={}
+    # converting JSON data to a dictionary 
+            if source.status_code == 200 and  source2.status_code == 200:
+                list_of_data = json.loads(source.content)
+                list_of_data2 = json.loads(source2.content)
+                print(type(list_of_data),'============================')
+                data = {
+                "country_code": str(list_of_data['sys']['country']), 
+                "coordinate": str(list_of_data['coord']['lon']) + ' '
+                        + str(list_of_data['coord']['lat']), 
+                "temp": str(list_of_data['main']['temp']) + 'k', 
+                "pressure": str(list_of_data['main']['pressure']), 
+                "humidity": str(list_of_data['main']['humidity']), 
+                }
+                if list_of_data2['alerts']['alerts'] == []:
+                            data2 = {
+                                "city": str(list_of_data2['alerts']['city']),
+                                "alerts": "Weather is not extreem in " + city
 
-    return render(request,'weather/home.html',{'data':data, 'data2' :data2})
+                            }
+            # print(list_of_data2)
+                else:
+                    data2 = {
+                        "city": str(list_of_data2['alerts']['city']),
+                        "alerts": str(list_of_data2['alerts']['alerts'][0]['description'] + '. Stay safe')
+
+                    }                
+        except requests.exceptions.HTTPError as err:
+            print('------------')
+            
+        except requests.exceptions.RequestException as e:
+            print(e)
+
+    return render(request,'weather/home.html',locals())
 
 def error(request):
-    return render(request,'weather/500.html')
+    return render(request, 'weather/500.html')
+
+
